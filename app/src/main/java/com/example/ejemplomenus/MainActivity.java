a18bf69f17bfe684265067dfe257956074235573
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-24 09:12:12 +0200

    Commit rama

--- app/src/main/java/com/example/ejemplomenus/MainActivity.java
+++ app/src/main/java/com/example/ejemplomenus/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        textView = findViewById(R.id.idTextView);
+        registerForContextMenu(textView);
     }
 
     @Override
@@ -28,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_opciones, menu);
         return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_opciones, menu);
     }
 
     @Override
@@ -41,6 +51,20 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.idMenu1:
+                Toast.makeText(this, "Pulsada primera opción", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.idMenu2:
+                Toast.makeText(this, "Pulsada segunda opción", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
         }
     }
 }
